f70ab0e128a230c109bb13c1b65bdf8b5d911901
Haneef Mubarak <haneef503@gmail.com>
2016-11-14 15:55:35 -0800

    [enlight.ml]: editor

---
+++ scratch/enlight.ml/editor/app.js
@@ -0,0 +1,11 @@
+document.getElementById ("note-title").innerHTML = localStorage["note-title"];
+document.getElementById ("note-content").innerHTML = localStorage["note-content"];
+
+setInterval (function () {
+	var title = document.getElementById ("note-title").innerHTML;
+
+	localStorage["note-title"] = title;
+	localStorage["note-content"] = document.getElementById ("note-content").innerHTML;
+
+	document.getElementsByTagName ("title")[0].innerHTML = "LiveNotes: " + title;
+}, 1000);
